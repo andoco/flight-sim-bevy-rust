140f31b75704bb23cca0d4072281e7c7a87533a7
Andrew O'Connor <andrew.oconnor@gmail.com>
2023-06-28 13:29:41 +0100

    Try a lookup table approach

--- src/plane.rs
+++ src/plane.rs
@@ -2,6 +2,7 @@ use core::f32;
 
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
+use enterpolation::{linear::Linear, Curve};
 use leafwing_input_manager::{
     prelude::{ActionState, DualAxis, InputManagerPlugin, InputMap},
     Actionlike, InputManagerBundle,
@@ -16,7 +17,14 @@ impl Plugin for PlanePlugin {
         app.add_plugin(InputManagerPlugin::<PlaneAction>::default())
             .add_startup_system(setup_plane)
             .add_system(add_plane_input)
-            .add_systems((handle_keyboard_input, compute_flight_dynamics).chain());
+            .add_systems(
+                (
+                    handle_keyboard_input,
+                    compute_flight_dynamics,
+                    compute_lookup_table_flight_dynamics,
+                )
+                    .chain(),
+            );
     }
 }
 
@@ -27,6 +35,12 @@ pub struct Plane;
 pub struct PlaneLimits {
     pub thrust: f32,
 }
+
+#[derive(Component)]
+pub struct EquationFlightDynamics;
+
+#[derive(Component)]
+pub struct LookupTableFlightDynamics;
 
 #[derive(Component, Default)]
 pub struct PlaneFlight {
@@ -46,6 +60,7 @@ fn setup_plane(
     commands
         .spawn((
             Plane,
+            LookupTableFlightDynamics,
             PlaneLimits { thrust: 150.0 },
             PlaneFlight::default(),
             SpatialBundle::from_transform(Transform::from_xyz(10., 1.1, 0.)),
@@ -76,7 +91,7 @@ fn setup_plane(
                     transform: Transform::from_xyz(0., 1.0, 0.),
                     ..default()
                 },
-                Collider::cuboid(5.0, 0.1, 1.0),
+                Collider::cuboid(7.5, 0.1, 1.0),
             ));
         });
 }
@@ -175,7 +190,7 @@ fn compute_flight_dynamics(
             &mut PlaneFlight,
             &mut ExternalForce,
         ),
-        With<Plane>,
+        With<EquationFlightDynamics>,
     >,
     rapier_config: Res<RapierConfiguration>,
 ) {
@@ -191,7 +206,7 @@ fn compute_flight_dynamics(
 
         let air_density = 1.225; // 1.225 kg/m^3 at sea level
         let dynamic_pressure = 0.5 * air_density * airspeed * airspeed;
-        let wing_area = 10.0 * 2.0; // length * width = area m^2
+        let wing_area = 15.0 * 2.0; // length * width = area m^2
 
         // Approximate lift coefficients For Cessna 172
         let lift_coefficient = match angle_of_attack.to_degrees() as i32 {
@@ -221,6 +236,70 @@ fn compute_flight_dynamics(
             drag,
             local_velocity.length(),
             airspeed
+        );
+
+        external_force.force = global_tx.forward() * flight.thrust;
+        external_force.force += -velocity.linvel.normalize_or_zero() * flight.drag;
+        external_force.force += global_tx.up() * flight.lift;
+    }
+}
+
+fn compute_lookup_table_flight_dynamics(
+    mut query: Query<
+        (
+            &GlobalTransform,
+            &Velocity,
+            &ReadMassProperties,
+            &mut PlaneFlight,
+            &mut ExternalForce,
+        ),
+        With<LookupTableFlightDynamics>,
+    >,
+    rapier_config: Res<RapierConfiguration>,
+) {
+    for (global_tx, velocity, ReadMassProperties(mass_props), mut flight, mut external_force) in
+        query.iter_mut()
+    {
+        let local_velocity = (global_tx.translation() + velocity.linvel) - global_tx.translation();
+        let airspeed = -local_velocity.z;
+
+        // Angle between the chord line of the wing (front edge to back edge) and the velocity
+        // of the air flowing over the wing.
+        let angle_of_attack = global_tx.forward().angle_between(velocity.linvel);
+
+        let air_density = 1.225; // 1.225 kg/m^3 at sea level
+        let dynamic_pressure = 0.5 * air_density * airspeed * airspeed;
+        let wing_area = 15.0 * 2.0; // length * width = area m^2
+
+        let lin = Linear::builder()
+            .elements([0.35, 0.7, 0.0])
+            .knots([0.0, 0.4, 1.0])
+            .build()
+            .unwrap();
+
+        let samples: Vec<_> = lin.take(10).collect();
+
+        let lift_coefficient = samples
+            .get((angle_of_attack.to_degrees() / 9.0) as usize)
+            .unwrap_or(&0.0);
+
+        let weight = rapier_config.gravity.y.abs() * mass_props.mass;
+
+        let lift = (lift_coefficient * dynamic_pressure * wing_area).min(weight * 2.0);
+
+        let drag_coefficient = 0.032; // For Cessna 172 at sea level and 100 knots at 0 degrees angle of attack
+        let drag = drag_coefficient * dynamic_pressure * wing_area;
+
+        flight.angle_of_attack = angle_of_attack;
+        flight.airspeed = airspeed;
+        flight.lift = lift;
+        flight.weight = weight;
+        flight.drag = drag;
+
+        info!(
+            "aoa={}, lc={}",
+            angle_of_attack.to_degrees(),
+            lift_coefficient,
         );
 
         external_force.force = global_tx.forward() * flight.thrust;
