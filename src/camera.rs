294478306c453bf10bda82fca29805a7e7c98b14
Andrew O'Connor <andrew.oconnor@gmail.com>
2023-06-29 17:45:42 +0100

    Add fog

--- src/camera.rs
+++ src/camera.rs
@@ -16,17 +16,29 @@ pub struct Follow;
 
 fn setup(mut commands: Commands) {
     commands
-        .spawn((Camera3dBundle {
-            camera_3d: Camera3d {
-                clear_color: ClearColorConfig::Custom(Color::rgb(0.5, 0.5, 0.8)),
+        .spawn((
+            Camera3dBundle {
+                camera_3d: Camera3d {
+                    clear_color: ClearColorConfig::Custom(Color::rgb(0.5, 0.5, 0.8)),
+                    ..default()
+                },
+                camera: Camera {
+                    order: 0,
+                    ..default()
+                },
                 ..default()
             },
-            camera: Camera {
-                order: 0,
-                ..default()
+            FogSettings {
+                color: Color::rgba(0.1, 0.2, 0.4, 1.0),
+                directional_light_color: Color::rgba(1.0, 0.95, 0.75, 0.5),
+                directional_light_exponent: 30.0,
+                falloff: FogFalloff::from_visibility_colors(
+                    1500.0, // distance in world units up to which objects retain visibility (>= 5% contrast)
+                    Color::rgb(0.35, 0.5, 0.66), // atmospheric extinction color (after light is lost due to absorption by atmospheric particles)
+                    Color::rgb(0.8, 0.844, 1.0), // atmospheric inscattering color (light gained due to scattering from the sun)
+                ),
             },
-            ..default()
-        },))
+        ))
         .insert(MainCamera);
 
     commands.spawn(Camera2dBundle {
